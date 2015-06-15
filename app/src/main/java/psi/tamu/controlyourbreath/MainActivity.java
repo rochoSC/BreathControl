05894a401110a44195d0a18a2c52745e1e64a7c4
Roger Solis Castilla <roger.fesca@gmail.com>
2015-06-15 14:43:01 -0500

    Count down chronometer added

--- app/src/main/java/psi/tamu/controlyourbreath/MainActivity.java
+++ app/src/main/java/psi/tamu/controlyourbreath/MainActivity.java
@@ -1,17 +1,30 @@
 package psi.tamu.controlyourbreath;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
 import zephyr.android.BioHarnessBT.*;
 
 public class MainActivity extends ActionBarActivity {
 
+    CounterDownChronometer crmt = null;
+    TextView txtCrmt = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtCrmt = (TextView) findViewById(R.id.txtChronometer);
+        crmt = new CounterDownChronometer(4000,1000);
+
     }
 
     @Override
@@ -35,4 +48,32 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClickStartBreathingCheckpoint(View v){
+        crmt.start();
+
+        //Check for Zephyr BioHarness
+    }
+
+    public class CounterDownChronometer extends CountDownTimer {
+        public CounterDownChronometer(long millisInFuture, long countDownInterval){
+            super(millisInFuture,countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            Log.i("Chronometer", "Changed to: " + hms);
+            txtCrmt.setText(hms);
+        }
+
+        @Override
+        public void onFinish() {
+            txtCrmt.setText("DONE!");
+        }
+    }
+
 }
